d5c9823cf6cc6af00a5f57d80741095d32678577
晨曦 <1179348863@qq.com>
2020-03-06 15:01:57 +0800

    练习4

--- Histogram/Histogram.cpp
+++ Histogram/Histogram.cpp
@@ -6,44 +6,36 @@ using namespace std;
 
 int main()
 {
-	cv::Mat src_colar = imread("E:\\4.png");//读取 图片 存入名为 src_color的Mat容器中
+	cv::Mat src_colar = imread("E:\\4.png");
+
+	std::vector<cv::Mat>channels;//声明vector，作为分离后3个通道图像的保存容器
+	split(src_colar, channels);//分离rgb三通道
+
+	cv::Mat B = channels.at(0);//获得3个通道的分离结果
+	cv::Mat G = channels.at(1);//三个通道的排列
+	cv::Mat R = channels.at(2);//顺序是B,G,R
+
+	Mat  dst;
+	//转换成灰度图像，使原图成为单通道图像
+	cvtColor(src_colar, dst, CV_BGR2GRAY);
+	//查看图像通道数
+	int c = dst.channels();
+	cout << "=" << endl << c << endl;
+
+	float histgram[256]= { 0 };
+		for (int j = 0; j < src_colar.rows; j++) {
+			uchar* data = src_colar.ptr <uchar>(j);
+			for (int i = 0; i < src_colar.cols; i++) {
+				//data[i] = j;
+				histgram[data[i]] = histgram[data[i]] + 1;
+			}
+		}
+	for (int i = 0; i < 256; i++) {
+    cout << "histgram" << histgram [i]<< endl;
+		}
 
 
-	vector<Mat>bgr_planes;
-	split(src_colar, bgr_planes);
-
-	//步骤二：计算直方图
-	int histsize = 256;
-	float range[] = { 0,256 };
-	const float*histRanges = { range };
-	Mat b_hist, g_hist, r_hist;
-	calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histsize, &histRanges, true, false);
-	calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histsize, &histRanges, true, false);
-	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histsize, &histRanges, true, false);
-
-	//归一化
-	int hist_h = 400;//直方图的图像的高
-	int hist_w = 512;////直方图的图像的宽
-	int bin_w = hist_w / histsize;//直方图的等级
-	Mat histImage(hist_w, hist_h, CV_8UC3, Scalar(0, 0, 0));//绘制直方图显示的图像
-	normalize(b_hist, b_hist, 0, hist_h, NORM_MINMAX, -1, Mat());//归一化
-	normalize(g_hist, g_hist, 0, hist_h, NORM_MINMAX, -1, Mat());
-	normalize(r_hist, r_hist, 0, hist_h, NORM_MINMAX, -1, Mat());
-
-	//步骤三：绘制直方图（render histogram chart）
-	for (int i = 1; i < histsize; i++)
-	{
-		//绘制蓝色分量直方图
-		line(histImage, Point((i - 1)*bin_w, hist_h - cvRound(b_hist.at<float>(i - 1))),
-			Point((i)*bin_w, hist_h - cvRound(b_hist.at<float>(i))), Scalar(255, 0, 0), 2, CV_AA);
-		//绘制绿色分量直方图
-		line(histImage, Point((i - 1)*bin_w, hist_h - cvRound(g_hist.at<float>(i - 1))),
-			Point((i)*bin_w, hist_h - cvRound(g_hist.at<float>(i))), Scalar(0, 255, 0), 2, CV_AA);
-		//绘制红色分量直方图
-		line(histImage, Point((i - 1)*bin_w, hist_h - cvRound(r_hist.at<float>(i - 1))),
-			Point((i)*bin_w, hist_h - cvRound(r_hist.at<float>(i))), Scalar(0, 0, 255), 2, CV_AA);
-	}
-	imshow("src", histImage);
+	imshow("src_colar", src_colar);
 	waitKey(0);//等待用户按键
 	return 0;
 }
