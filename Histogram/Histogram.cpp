53507f91bfe411eaf704bb0d244ac2c4a42b533d
晨曦 <1179348863@qq.com>
2020-03-08 14:39:14 +0800

    练习5

--- Histogram/Histogram.cpp
+++ Histogram/Histogram.cpp
@@ -9,7 +9,7 @@ int main()
 	cv::Mat src_colar = imread("E:\\4.png");
 	std::vector<cv::Mat>channels;
 	Mat  dst;
-	//转换成灰度图像，使原图成为单通道图像
+	//转换成灰度图像，使原图成为单通道图像,或imread（"",0）;
 	cvtColor(src_colar, dst, CV_BGR2GRAY);
 	//查看图像通道数
 	int c = dst.channels();
@@ -17,7 +17,7 @@ int main()
 
 	float histgram[256]= { 0 };
 		for (int j = 0; j < dst.rows; j++) {
-			uchar* data = src_colar.ptr <uchar>(j);
+			uchar* data = dst.ptr <uchar>(j);
 			for (int i = 0; i < dst.cols; i++) {
 				//data[i] = j;
 				histgram[data[i]] = histgram[data[i]] + 1;
@@ -34,25 +34,25 @@ int main()
 	int hist_w = 512;////直方图的图像的宽
 	Mat histImage(hist_w, hist_h, CV_8UC3, Scalar(0, 0, 0));//绘制直方图显示的图像
 
-    //draw rectangle  
+    /*draw rectangle  
 	cv::Rect rect;
 	rect.x = 0;
 	rect.y = 0;
 	rect.width = 2;
 	for (int i = 0; i < 256; i++) {
 		rect.x = rect.x + 2;
-		rect.height = histgram[i]*3000;	
+		rect.height = histgram[i]*28000;	
 		rectangle(histImage, rect, CV_RGB(255, 0, 0), 1, 8, 0);
-	}
+	}//            */
 
-	/*draw line
+	//*draw line
 	Point pt1, pt2;
 	pt1.x = 0;
 	pt1.y = 399;
 	pt2.x = 0;
 	pt2.y = 0;
 	for (int i = 0; i < 256; i++) {
-		pt2.y = pt2.y-histgram[i] * 3000;
+		pt2.y = pt1.y-histgram[i] * 20000;
 
 		pt2.x = pt2.x + 1;
 		pt1.x = pt2.x ;
